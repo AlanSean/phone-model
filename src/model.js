59460423f72d5c7646550835dc798daea30715e6
huangqing <2316694914@qq.com>
2021-01-07 11:59:13 +0800

    更新 苹果12系列的参数

--- src/model.js
+++ src/model.js
@@ -4,19 +4,19 @@ export default {
             model: ['iPhone 4', 'iPhone 4s'],
             statusHeight: 20,//状态栏
             // navBarHeight: 44,//非通用
-            insetBottom: 34//底部栏
+            insetBottom: 0//底部安全区域
         },
         "640x1136": {
             model: ['iPhone 5', 'iPhone 5s', 'iPhone 5c', 'iPhone SE'],
             statusHeight: 20,
             // navBarHeight: 44,
-            insetBottom: 34
+            insetBottom: 0
         },
         "750x1334": {
             model: ['iPhone 6', 'iPhone 6s', 'iPhone 7', 'iPhone 8'],
             statusHeight: 20,
             // navBarHeight: 44,
-            insetBottom: 34
+            insetBottom: 0
         },
         '828x1792': {
             model: ['iPhone Xr', 'iPhone 11'],
@@ -36,7 +36,7 @@ export default {
             model: ['iPhone 6 Plus', 'iPhone 6s Plus', 'iPhone 7 Plus', 'iPhone 8 Plus'],
             statusHeight: 20,
             // navBarHeight: 44,
-            insetBottom: 34
+            insetBottom: 0
         },
         '1242x2688': {
             model: ['iPhone Xs Max', 'iPhone 11 Pro Max'],
@@ -44,5 +44,23 @@ export default {
             // navBarHeight: 44,
             insetBottom: 34
         },
+        '1080x2340':{
+            model: ['iPhone 12 mini'],
+            statusHeight: 44,
+            // navBarHeight: 44,
+            insetBottom: 34
+        },
+        '1170x2531':{
+            model: ['iPhone 12 Pro', 'iPhone 12'],
+            statusHeight: 44,
+            // navBarHeight: 44,
+            insetBottom: 34
+        },
+        '1284x2778':{
+            model: ['iPhone 12 Pro Max'],
+            statusHeight: 44,
+            // navBarHeight: 44,
+            insetBottom: 34
+        }
     }
 }
